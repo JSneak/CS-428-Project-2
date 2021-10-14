8726ec8c97f5515feaab523c0f60698d12c2700a
kaowayne8 <wkao9@uic.edu>
2021-10-14 13:30:38 -0500

    V1 script digital time

---
+++ Assets/DigitalTimeChanger.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.Networking;
+using TMPro;
+using System;
+
+public class DigitalTimeChanger : MonoBehaviour
+{
+
+    public int hour = 9;
+    public int minute = 0;
+    public string timeOfDay = "AM";
+    public GameObject textMesh;
+       
+    // Start is called before the first frame update
+    void Start()
+    {
+        //1 1.167
+        InvokeRepeating("ChangeTime", 0.1f, 0.23f);
+
+    }
+
+    void ChangeTime(){
+        if(minute == 59){
+            minute = 0;
+
+            if(hour+1 == 12){
+                if(timeOfDay == "AM"){
+                    timeOfDay = "PM";
+                }
+                else{
+                    timeOfDay = "AM";
+                }
+            }
+            
+            if(hour+1 > 12){
+                hour = 1;
+            }
+            else{
+                hour++;
+            }
+        }
+        else{
+            minute++;
+        }
+
+        string time = hour.ToString() + ":" + minute.ToString() + timeOfDay;
+        textMesh.GetComponent<TextMeshPro>().text = time;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
